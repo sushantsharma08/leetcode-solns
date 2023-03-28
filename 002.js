46da3b132f64b9b4a4ac485175b500415dbbcedc
Sushant Sharma <ssushantlb08@gmail.com>
2023-03-28 12:31:08 +0530

    ques done

---
+++ 002.js
@@ -0,0 +1,20 @@
+// EASY
+
+// Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+ 
+// Example :
+
+// Input: nums = [0,2,1,5,3,4]
+// Output: [0,1,2,4,5,3]
+
+
+var buildArray = function(nums) {
+    let ans=[];
+    for(let i=0;i<=nums.length-1;i++){
+    ans.push(nums[nums[i]])
+    }
+    return ans;
+};
